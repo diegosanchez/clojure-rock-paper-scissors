2dd0f990db2cafd3b3bb7c38a4ed6377b33eac3a
Diego Sanchez <diego.sanches@gmail.com>
2020-08-21 17:29:57 -0300

    Sissors agains paper

--- test/kata/rock_paper_scissors_test.clj
+++ test/kata/rock_paper_scissors_test.clj
@@ -1,8 +1,14 @@
 (ns kata.rock-paper-scissors-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.test :refer :al]l
             [kata.rock-paper-scissors :refer :all]))
 
-(deftest a-test-0
+(deftest sissors-test
   (testing "Winner"
-    (is (= :sissors (winner :scissors :paper)))))
+    (is (= :sissors (winner :scissors :paper)))
+    (is (= :sissors (winner :paper :scissors)))))
+
+;; (deftest rock-test
+;;   (testing "Winner"
+;;     (is (= :rock (winner :rock :sissors)))
+;;     (is (= :rock (winner :scissors :rock)))))
 
